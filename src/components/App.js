a5dd0e758ac548d0ba72cf1da77638c7c172af94
Aleksefo <aleksefo@gmail.com>
2017-03-24 15:37:42 +0200

    React Navigation modified

--- src/components/App.js
+++ src/components/App.js
@@ -4,43 +4,12 @@ import {
 	Text, View,
 } from 'react-native'
 import { StackNavigator } from 'react-navigation'
-
-class HomeScreen extends Component {
-	static navigationOptions = {
-		title: 'Welcome to UltimateAchiever',
-	};
-	render() {
-		const { navigate } = this.props.navigation;
-		return (
-			<View>
-				<Text>Hello, Chat App!</Text>
-				<Button
-					onPress={() => navigate('Chat', { user: 'John' })}
-					title="Chat with Lucy"
-				/>
-			</View>
-		);
-	}
-}
-class ChatScreen extends React.Component {
-	static navigationOptions = {
-		// Nav options can be defined as a function of the navigation prop:
-		title: ({ state }) => `Chat with ${state.params.user}`,
-	};
-	render() {
-		// The screen's current route is passed in to `props.navigation.state`:
-		const { params } = this.props.navigation.state;
-		return (
-			<View>
-				<Text>Chat with {params.user}</Text>
-			</View>
-		);
-	}
-}
+import PomoScreen from './Pomodoro/PomoScreen'
+import HomeScreen from './Home/HomeScreen'
 
 const UltimateAchiever = StackNavigator({
 	Home: { screen: HomeScreen },
-	Chat: { screen: ChatScreen },
-});
+	Pomo: { screen: PomoScreen },
+})
 
-AppRegistry.registerComponent('UltimateAchiever', () => UltimateAchiever);
+AppRegistry.registerComponent('UltimateAchiever', () => UltimateAchiever)
--- src/components/Pomodoro/PomoScreen.js
+++ src/components/Pomodoro/PomoScreen.js
@@ -11,21 +11,24 @@ import Countdown from './Countdown'
 import Header from '../common/Header'
 
 
-export default class Pomodoro extends Component {
-	// let timeSession = 5
-
+export default class PomoScreen extends Component {
+	static navigationOptions = {
+		// Nav options can be defined as a function of the navigation prop:
+		title: ({ state }) => `Chat with ${state.params.user}`,
+	}
 	render() {
+		// The screen's current route is passed in to `props.navigation.state`:
+		const { params } = this.props.navigation.state
 		return (
 			<View >
 				<Header headerText={'Pomodoro'}/>
 				<Countdown time={10}/>
+				<Text>Chat with {params.user}</Text>
 			</View>
 		)
 	}
 
 }
-
-
 //
 // const styles = StyleSheet.create({
 // 	container: {
